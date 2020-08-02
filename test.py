38596e36faabe1e3db3042e38e191de93f81c79c
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-08-02 12:05:39 -0700

    commit

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ def test():
 	# testPost(webgram.get('freedom_watch'))
 	# [testPost(post) for post in webgram.getPosts('equality_and_rights', 1142)]
 	# testPost(webgram.getPost('equality_and_rights', 6371))
-	[testPost(post) for post in webgram.getPosts('solidot', 7585, direction='before')]
+	[testPost(post) for post in webgram.getPosts('solidot', 7600, direction='before')]
 	# [testPost(post) for post in webgram.getPosts('freedom_watch')]
 
 if __name__=='__main__':
