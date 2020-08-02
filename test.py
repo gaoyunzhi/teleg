731c234df68d68f5f1d72526bc1f90440a4f31c0
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-08-02 12:36:30 -0700

    commit

--- test.py
+++ test.py
@@ -6,16 +6,20 @@ import webgram
 def testPost(post):
 	# print(post)
 	# print(post.getIndex())
-	print(post.getMaintext())
+	# print(post.getMaintext())
 	print(post.getKey())
 	# print(post.__dict__)
 	# print(list(webgram.yieldReferers(post)))
+	webgram.yieldReferers(post)
 
 def test():
 	# testPost(webgram.get('freedom_watch'))
 	# [testPost(post) for post in webgram.getPosts('equality_and_rights', 1142)]
 	# testPost(webgram.getPost('equality_and_rights', 6371))
-	[testPost(post) for post in webgram.getPosts('solidot', 7600, direction='before')]
+	[testPost(post) for post in webgram.getPosts('Pinwan', 1)]
+	[testPost(post) for post in webgram.getPosts('Pinwan', 25)]
+	[testPost(post) for post in webgram.getPosts('Pinwan', 45)]
+	[testPost(post) for post in webgram.getPosts('Pinwan', 65)]
 	# [testPost(post) for post in webgram.getPosts('freedom_watch')]
 
 if __name__=='__main__':
