f9bf6ae7352d69d7b1623d82e50f228f60ce07a2
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-08-02 13:32:56 -0700

    commit

--- test.py
+++ test.py
@@ -10,14 +10,15 @@ def testPost(post):
 	print(post.getKey())
 	# print(post.__dict__)
 	# print(list(webgram.yieldReferers(post)))
-	list(webgram.yieldReferers(post))
+	# list(webgram.yieldReferers(post))
 
 def test():
 	# testPost(webgram.get('freedom_watch'))
 	# [testPost(post) for post in webgram.getPosts('equality_and_rights', 1142)]
 	# testPost(webgram.getPost('equality_and_rights', 6371))
-	[testPost(post) for post in webgram.getPosts('Pinwan', 1)]
+	[testPost(post) for post in webgram.getPosts('Pinwan', 100000, direction='after')]
 	# [testPost(post) for post in webgram.getPosts('freedom_watch')]
+	# print(len(webgram.getPosts('Pinwan', 100000, direction='after')))
 
 if __name__=='__main__':
 	test()
